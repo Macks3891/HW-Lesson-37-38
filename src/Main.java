6f8095c8e03d737f1e4bb6e01fd32513f05a4c06
Максим <166473866+Macks3891@users.noreply.github.com>
2024-11-01 23:03:24 +0500

    HW Player 1 Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+/**
+ * Главный класс, демонстрирующий использование класса Player.
+ */
 public class Main {
     public static void main(String[] args) {
+        Player[] players = new Player[6];
 
+        for (int i = 0; i < 6; i++) {
+            players[i] = new Player();
+        }
+        Player.info();
+
+        Player extraPlayer = new Player();
+        Player.info();
+
+        for (Player player : players) {
+            while (player.getStamina() > 0) {
+                player.run();
+            }
+        }
+
+        System.out.println("Количество игроков на поле: " + Player.getCountPlayers());
     }
 }
